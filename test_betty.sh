94aab8bf0718968445320454741097e08b08a580
Miniknacky <jonathanhuybrechts@gmail.com>
2023-12-22 12:05:15 +0000

    erase last

--- test_betty.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Parcourir chaque fichier .c et .h dans le dossier courant
-find . -name "*.c" -o -name "*.h" | while read file; do
-    echo "Vérification de $file"
-    betty "$file"
-done
